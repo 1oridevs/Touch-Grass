cb2c313b698a089ec44db1d9913ecb53ada55edb
Ori Cohen <143204565+1oridevs@users.noreply.github.com>
2025-01-09 21:18:47 +0200

    Delete target directory

--- target/debug/deps/touch_grass.d
+++
@@ -1,6 +0,0 @@
-C:\Users\origa\Desktop\Projects\TG\touch-grass\target\debug\deps\touch_grass.exe: src/main.rs src\token.rs
-
-C:\Users\origa\Desktop\Projects\TG\touch-grass\target\debug\deps\touch_grass.d: src/main.rs src\token.rs
-
-src/main.rs:
-src\token.rs:
--- target/debug/touch-grass.d
+++
@@ -1,1 +0,0 @@
-C:\Users\origa\Desktop\Projects\TG\touch-grass\target\debug\touch-grass.exe: C:\Users\origa\Desktop\Projects\TG\touch-grass\src\main.rs C:\Users\origa\Desktop\Projects\TG\touch-grass\src\token.rs
